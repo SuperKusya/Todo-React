92658fe747fbd95a7df79ef5337d388d6834879d
Arthur Galiullin <galiullinko@gmail.com>
2020-03-13 15:47:13 +0300

    Add styles, change structure

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import AppHeader from './components/app-header'
-import TodoList from './components/todo-list';
-
-const App = () => {
-    const todoData = [
-        { label: 'Drink Coffee', important: false },
-        { label: 'Make Aws App', important: true },
-        { label: 'Have a lunch', important: false },
-    ];
-
-    return (
-        <div>
-           <AppHeader />
-           <TodoList todos={todoData}/>
-        </div>
-    );
-}
+import App from './components/app';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
